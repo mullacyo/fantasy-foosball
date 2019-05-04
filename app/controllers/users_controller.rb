bc70b55e8470b8e4a27b62dab7343f8418595637
Callum Tan <mullacyo@hotmail.com>
2019-05-05 05:26:07 +0800

    added create user feature.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path
+        else
+            flash[:message] = "Error, please try again."
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name)
+    end
 end
